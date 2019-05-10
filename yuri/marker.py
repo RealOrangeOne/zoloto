bfaf4cf6b93c36b61d59a47eb23d7ccea8838179
Jake Howard <git@theorangeone.net>
2019-05-10 19:19:21 +0100

    Convert marker attributes to private values

--- yuri/marker.py
+++ yuri/marker.py
@@ -8,27 +8,35 @@ class Marker:
     def __init__(
         self, id: int, corners, size, calibration_params: CalibrationParameters
     ):
-        self.id = id
-        self.pixel_corners = corners
-        self.size = size
-        self._camera_calibration_params = calibration_params
+        self.__id = id
+        self.__pixel_corners = corners
+        self.__size = size
+        self.__camera_calibration_params = calibration_params
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def size(self):
+        return self.__size
 
     @cached_property
-    def _vectors(self):
+    def __vectors(self):
         rvec, tvec, _ = aruco.estimatePoseSingleMarkers(
-            self.pixel_corners,
-            self.size,
-            self._camera_calibration_params.camera_matrix,
-            self._camera_calibration_params.distance_coefficients,
+            self.__pixel_corners,
+            self.__size,
+            self.__camera_calibration_params.camera_matrix,
+            self.__camera_calibration_params.distance_coefficients,
         )
         return rvec, tvec
 
     @cached_property
     def rvec(self):
-        rvec, _ = self._vectors
+        rvec, _ = self.__vectors
         return rvec
 
     @cached_property
     def tvec(self):
-        _, tvec = self._vectors
+        _, tvec = self.__vectors
         return tvec
