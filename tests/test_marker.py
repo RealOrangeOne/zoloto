b5dc8b69a0851a6a9e2a78dbf4ab7fbcbdef8ed7
Jake Howard <git@theorangeone.net>
2019-06-01 14:13:34 +0100

    Run black on missed change

--- tests/test_marker.py
+++ tests/test_marker.py
@@ -76,7 +76,9 @@ class MarkerTestCase(TestCase):
     def test_many_as_ujson(self):
         created_markers_dict = ujson.loads(ujson.dumps(self.markers))
         self.assertEqual(len(created_markers_dict), 1)
-        self.assertEqual({marker['id'] for marker in created_markers_dict}, {self.MARKER_ID})
+        self.assertEqual(
+            {marker["id"] for marker in created_markers_dict}, {self.MARKER_ID}
+        )
 
     def test_dict_as_ujson(self):
         marker_dict = self.marker.as_dict()
