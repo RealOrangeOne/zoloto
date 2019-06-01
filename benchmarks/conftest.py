0e6627dca5565b4b0a01fd8a7917e66b814fda44
Jake Howard <git@theorangeone.net>
2019-06-01 13:41:52 +0100

    Merge pytest fixtures from tests and benchmarks

--- benchmarks/conftest.py
+++ benchmarks/conftest.py
@@ -1,14 +1,1 @@
-import pytest
-from cv2 import aruco
-
-from yuri.camera import MarkerCamera
-
-
-@pytest.fixture
-def marker_camera():
-    return MarkerCamera(25, marker_dict=aruco.DICT_6X6_50, marker_size=200)
-
-
-@pytest.fixture
-def marker(marker_camera):
-    return next(marker_camera.process_frame())
+from tests.conftest import *  # noqa
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,10 @@ import os
 from tempfile import mkstemp
 
 import pytest
+from cv2 import aruco
+
+from yuri.calibration import get_fake_calibration_parameters
+from yuri.camera import MarkerCamera
 
 
 @pytest.fixture
@@ -21,3 +25,18 @@ def make_temp_file(request):
 
     request.addfinalizer(clean_temp_file)
     return _make_temp_file
+
+
+@pytest.fixture
+def marker_camera():
+    return MarkerCamera(25, marker_dict=aruco.DICT_6X6_50, marker_size=200)
+
+
+@pytest.fixture
+def marker(marker_camera):
+    return next(marker_camera.process_frame())
+
+
+@pytest.fixture
+def fake_calibration_params():
+    return get_fake_calibration_parameters(200)
