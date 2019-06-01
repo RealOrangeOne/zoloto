1efd4e6274c020b630bc351ea86e408a65f64b81
Jake Howard <git@theorangeone.net>
2019-06-01 13:43:31 +0100

    Add benchmarks for calibration files

---
+++ benchmarks/test_calibration.py
@@ -0,0 +1,27 @@
+from functools import partial
+
+from yuri.calibration import parse_calibration_file, save_calibrations
+
+
+def test_save_calibrations_json(benchmark, fake_calibration_params, make_temp_file):
+    benchmark(
+        partial(save_calibrations, fake_calibration_params, make_temp_file(".json"))
+    )
+
+
+def test_save_calibrations_xml(benchmark, fake_calibration_params, make_temp_file):
+    benchmark(
+        partial(save_calibrations, fake_calibration_params, make_temp_file(".xml"))
+    )
+
+
+def test_parse_calibrations_xml(benchmark, fake_calibration_params, make_temp_file):
+    temp_file = make_temp_file(".xml")
+    save_calibrations(fake_calibration_params, temp_file)
+    benchmark(partial(parse_calibration_file.__wrapped__, temp_file))
+
+
+def test_parse_calibrations_json(benchmark, fake_calibration_params, make_temp_file):
+    temp_file = make_temp_file(".json")
+    save_calibrations(fake_calibration_params, temp_file)
+    benchmark(partial(parse_calibration_file.__wrapped__, temp_file))
