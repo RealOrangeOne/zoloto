1b45685d07b3dcbb90d9e7b2fc59eefbaae410eb
Jake Howard <git@theorangeone.net>
2019-06-06 17:30:15 +0100

    Just ignore the print statement, not the whole line!

--- examples/count-markers.py
+++ examples/count-markers.py
@@ -6,4 +6,4 @@ camera = Camera(0, marker_dict=aruco.DICT_6X6_50)
 
 while True:
     marker_ids = camera.get_visible_markers()
-    print("I can see {} markers".format(len(marker_ids)), end="\r")  # noqa: F401
+    print("I can see {} markers".format(len(marker_ids)), end="\r")  # noqa: T001
