8e3270b360319fc800fafcd5477422dbe8de7b41
Jake Howard <git@theorangeone.net>
2019-06-06 17:46:57 +0100

    Benchmark saving frame

--- benchmarks/test_marker_camera.py
+++ benchmarks/test_marker_camera.py
@@ -15,3 +15,11 @@ def test_capture_frame(benchmark, marker_camera):
 def test_get_visible_markers(benchmark, marker_camera):
     frame = marker_camera.capture_frame()
     benchmark(marker_camera.get_visible_markers, frame=frame)
+
+
+def test_save_frame(benchmark, marker_camera, make_temp_file):
+    benchmark(marker_camera.save_frame, make_temp_file(".png"))
+
+
+def test_save_frame_with_annotation(benchmark, marker_camera, make_temp_file):
+    benchmark(marker_camera.save_frame, make_temp_file(".png"), annotate=True)
