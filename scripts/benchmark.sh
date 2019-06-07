cf838a79bec5d15ad0b6591ab91d0f7cc96965db
Jake Howard <git@theorangeone.net>
2019-06-07 22:57:30 +0100

    Improve output of benchmarks
    We lose the auto-discovery feature, but gain better comparison of cameras, and better grouping of results

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -4,5 +4,15 @@ set -e
 
 export PATH=env/bin:${PATH}
 
-echo "> Running benchmarks..."
-pytest --verbose --benchmark-sort=fullname --benchmark-name=long --benchmark-columns=min,max,mean,stddev,ops benchmarks/
+run_benchmark() {
+    pytest --benchmark-sort=fullname --benchmark-name=long --benchmark-columns=min,max,mean,stddev,ops $@
+}
+
+echo "> Running calibration benchmarks..."
+run_benchmark benchmarks/test_calibration.py
+
+echo "> Running camera benchmarks..."
+run_benchmark benchmarks/test_*_camera.py --benchmark-group-by=name
+
+echo "> Running marker benchmarks..."
+run_benchmark benchmarks/test_marker.py
