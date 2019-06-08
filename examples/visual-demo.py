01fb07ae5f52f2a85c532f1de72ef9ff5d036906
Jake Howard <git@theorangeone.net>
2019-06-08 12:56:49 +0100

    Add FPS counter

--- examples/visual-demo.py
+++ examples/visual-demo.py
@@ -3,6 +3,8 @@ This example required `opencv-contrib-python` rather than `opencv-contrib-python
 """
 
 
+import datetime
+
 from cv2 import imshow, waitKey
 from cv2.aruco import DICT_6X6_50
 
@@ -10,9 +12,20 @@ from yuri.cameras.camera import Camera
 
 camera = Camera(0, marker_dict=DICT_6X6_50)
 
+last_time = datetime.datetime.today().timestamp()
+diffs = []
 
 while True:
     frame = camera.capture_frame()
     camera._annotate_frame(frame)
     imshow("demo", frame)
     waitKey(1)
+
+    new_time = datetime.datetime.today().timestamp()
+    diffs.append(new_time - last_time)
+    last_time = new_time
+
+    if len(diffs) > 10:
+        diffs = diffs[-10:]
+
+    print(int(len(diffs) / sum(diffs)), end="\r")
