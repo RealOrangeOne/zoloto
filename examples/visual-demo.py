b43da384a5b850595535a46c0894e9ee240e24ba
Jake Howard <git@theorangeone.net>
2019-06-08 13:16:04 +0100

    Just time it rather than do a fancy FPS

--- examples/visual-demo.py
+++ examples/visual-demo.py
@@ -2,9 +2,7 @@
 This example required `opencv-contrib-python` rather than `opencv-contrib-python-headless`
 """
 
-
-import datetime
-
+from chrono import Timer
 from cv2 import imshow, waitKey
 from cv2.aruco import DICT_6X6_50
 
@@ -12,20 +10,15 @@ from yuri.cameras.camera import Camera
 
 camera = Camera(0, marker_dict=DICT_6X6_50)
 
-last_time = datetime.datetime.today().timestamp()
-diffs = []
-
 while True:
-    frame = camera.capture_frame()
-    camera._annotate_frame(frame)
+    with Timer() as capture_timer:
+        frame = camera.capture_frame()
+    with Timer() as annotate_timer:
+        camera._annotate_frame(frame)
     imshow("demo", frame)
     waitKey(1)
-
-    new_time = datetime.datetime.today().timestamp()
-    diffs.append(new_time - last_time)
-    last_time = new_time
-
-    if len(diffs) > 10:
-        diffs = diffs[-10:]
-
-    print(int(len(diffs) / sum(diffs)), end="\r")
+    print(  # noqa: T001
+        round(capture_timer.elapsed * 1000),
+        round(annotate_timer.elapsed * 1000),
+        end="\r",
+    )
