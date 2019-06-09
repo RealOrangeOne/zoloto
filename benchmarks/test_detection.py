af44bd34268e69c86481c211a1f10dea21965565
Jake Howard <git@theorangeone.net>
2019-06-09 17:43:38 +0100

    Add benchmarks using test images

---
+++ benchmarks/test_detection.py
@@ -0,0 +1,41 @@
+import os
+
+import pytest
+from cv2.aruco import DICT_APRILTAG_36H11
+
+from tests.conftest import IMAGE_DATA, TEST_DATA_DIR
+from yuri.cameras.file import ImageFileCamera
+
+
+@pytest.fixture(params=IMAGE_DATA.keys())
+def image_camera(request):
+    return ImageFileCamera(
+        os.path.join(TEST_DATA_DIR, request.param), marker_dict=DICT_APRILTAG_36H11
+    )
+
+
+def test_get_visible_markers(benchmark, image_camera):
+    benchmark(image_camera.get_visible_markers)
+
+
+def test_save_frame(benchmark, image_camera, make_temp_file):
+    benchmark(image_camera.save_frame, make_temp_file(".png"))
+
+
+def test_save_frame_with_annotation(benchmark, image_camera, make_temp_file):
+    benchmark(image_camera.save_frame, make_temp_file(".png"), annotate=True)
+
+
+@pytest.mark.parametrize("filename", IMAGE_DATA.keys())
+def test_process_frame(filename, benchmark, make_temp_file):
+    class TestCamera(ImageFileCamera):
+        def get_marker_size(self, id):
+            return 100
+
+        def get_calibrations(self):
+            return None
+
+    camera = TestCamera(
+        os.path.join(TEST_DATA_DIR, filename), marker_dict=DICT_APRILTAG_36H11
+    )
+    benchmark(camera.save_frame, make_temp_file(".png"))
