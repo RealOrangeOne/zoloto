09bb150c3e5dcec4329eeb14475ae0e804a381f1
Jake Howard <git@theorangeone.net>
2019-06-09 17:43:26 +0100

    Remove unnecessary benchmark grouping

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -4,15 +4,4 @@ set -e
 
 export PATH=env/bin:${PATH}
 
-run_benchmark() {
-    pytest --benchmark-sort=fullname --benchmark-name=long --benchmark-columns=min,max,mean,stddev,ops $@
-}
-
-echo "> Running calibration benchmarks..."
-run_benchmark benchmarks/test_calibration.py
-
-echo "> Running camera benchmarks..."
-run_benchmark benchmarks/test_*_camera.py --benchmark-group-by=name
-
-echo "> Running marker benchmarks..."
-run_benchmark benchmarks/test_marker.py
+pytest --verbose --benchmark-sort=fullname --benchmark-columns=min,max,mean,stddev,ops benchmarks/ --benchmark-group-by=func --benchmark-name=long
