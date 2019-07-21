55a3c95a32af9eb32b639e2426cf78a055a8e219
Jake Howard <git@theorangeone.net>
2019-07-21 16:30:37 +0100

    Fix type issue in image file camera example

--- examples/annotate-image.py
+++ examples/annotate-image.py
@@ -3,7 +3,7 @@ import sys
 from zoloto.cameras.file import ImageFileCamera
 from zoloto.marker_dict import MarkerDict
 
-with ImageFileCamera(sys.argv[1], marker_dict=MarkerDict.DICT_6X6_50) as camera:
+with ImageFileCamera(str(sys.argv[1]), marker_dict=MarkerDict.DICT_6X6_50) as camera:
     camera.save_frame(sys.argv[2], annotate=True)
     print(  # noqa: T001
         "Saw {} markers in this image".format(len(camera.get_visible_markers()))
