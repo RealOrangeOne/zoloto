f183f139340d9d9374b58afd529d859a5b80ea29
Dan Trickey <dan@trickey.io>
2019-07-22 11:24:59 +0100

    Replace setup.py and friends with PyPoetry
    This will resolve all of the issues with development dependency resolution.
    
    Other features of this toolings that will improve the dev lifecyle include:
    
    - Separately pinned development dependencies in poetry.lock
    - Single command deployment to PyPI
    - All config in one, standardised file (pyproject.toml)
    - Prod dependencies are specified using caret notation, making it more suitable for PyPI
    - All of the features of the previous setup are still present.
    - Pipenv style integrated virtualenv
    - Old style files can be generated with `poetry export`

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -2,10 +2,4 @@
 
 set -e
 
-export PATH=env/bin:${PATH}
-
-rm -rf dist/*
-
-pip install wheel
-
-python setup.py sdist bdist_wheel
+poetry publish --build
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,12 +2,7 @@
 
 set -e
 
-export PATH=env/bin:${PATH}
-
-python3 -m venv env
-
-pip install -e .
-pip install -r dev-requirements.txt
+poetry install
 
 # `black` isn't available on Python3.5, so ignore if install fails
 pip install black==19.3b0 || true
