313924b6a5896695da458601039e0632db7828b8
Jake Howard <git@theorangeone.net>
2020-01-14 20:59:58 +0000

    Update preview CLI

--- zoloto/cli/preview.py
+++ zoloto/cli/preview.py
@@ -1,9 +1,8 @@
 import argparse
 
-import cv2
-
 from zoloto.cameras.camera import Camera
 from zoloto.marker_dict import MarkerDict
+from zoloto.viewer import CameraViewer
 
 
 def main():
@@ -11,11 +10,7 @@ def main():
     parser.add_argument("--id", type=int, default=0)
     args = parser.parse_args()
     with Camera(args.id, marker_dict=MarkerDict.DICT_6X6_250) as camera:
-        while True:
-            frame = camera.capture_frame()
-            camera._annotate_frame(frame)
-            cv2.imshow("Camera", frame)
-            cv2.waitKey(1)
+        CameraViewer(camera).start()
 
 
 if __name__ == "__main__":
