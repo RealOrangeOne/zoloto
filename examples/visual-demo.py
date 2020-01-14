1223142e32b56219de2121286c2b83f70778a49e
Jake Howard <git@theorangeone.net>
2020-01-14 20:59:47 +0000

    Update visual demo example

--- examples/visual-demo.py
+++ examples/visual-demo.py
@@ -1,28 +1,23 @@
 from chrono import Timer
-from cv2 import imshow, waitKey
 
-from zoloto import assert_has_gui_components
 from zoloto.cameras.camera import Camera
 from zoloto.marker_dict import MarkerDict
-
-assert_has_gui_components()
+from zoloto.viewer import CameraViewer
 
 
 class TestCamera(Camera):
     pass
 
 
+class Viewer(CameraViewer):
+    def on_frame(self, frame):
+        with Timer() as annotate_timer:
+            frame = camera._annotate_frame(frame)
+        print(round(annotate_timer.elapsed * 1000), end="\r")  # noqa: T001
+        return frame
+
+
 camera = TestCamera(0, marker_dict=MarkerDict.DICT_6X6_50)
 
-while True:
-    with Timer() as capture_timer:
-        frame = camera.capture_frame()
-    with Timer() as annotate_timer:
-        camera._annotate_frame(frame)
-    imshow("demo", frame)
-    waitKey(1)
-    print(  # noqa: T001
-        round(capture_timer.elapsed * 1000),
-        round(annotate_timer.elapsed * 1000),
-        end="\r",
-    )
+
+Viewer(camera).start()
