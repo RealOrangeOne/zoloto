b2081338f526caaa8ee34a2a26a15273043275f0
Jake Howard <git@theorangeone.net>
2020-01-14 22:00:09 +0000

    Add a test for abstract methods

--- tests/test_camera/test_camera_module.py
+++ tests/test_camera/test_camera_module.py
@@ -12,3 +12,15 @@ def test_exposes_file_camera(camera_name):
     assert getattr(zoloto.cameras, camera_name) == getattr(
         zoloto.cameras.file, camera_name
     )
+
+
+def test_requires_abstract_arguments():
+    class TestCamera(zoloto.cameras.Camera):
+        pass
+
+    with pytest.raises(TypeError) as e:
+        TestCamera(0)
+    assert (
+        "Can't instantiate abstract class TestCamera with abstract methods"
+        in e.value.args[0]
+    )
