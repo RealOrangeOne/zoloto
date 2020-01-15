dc19f9f3119fadf38ae4914f2cd4570165c1d5d1
Jake Howard <git@theorangeone.net>
2020-01-15 09:26:02 +0000

    Test marker camera doesn't need `get_marker_size`

--- tests/test_camera/test_camera_module.py
+++ tests/test_camera/test_camera_module.py
@@ -14,13 +14,31 @@ def test_exposes_file_camera(camera_name):
     )
 
 
-def test_requires_abstract_arguments():
+def test_camera_requires_abstract_arguments():
     class TestCamera(zoloto.cameras.Camera):
         pass
 
     with pytest.raises(TypeError) as e:
         TestCamera(0)
+    exception_message = e.value.args[0]
     assert (
         "Can't instantiate abstract class TestCamera with abstract methods"
-        in e.value.args[0]
+        in exception_message
     )
+    assert "marker_dict" in exception_message
+    assert "get_marker_size" in exception_message
+
+
+def test_marker_camera_requires_abstract_arguments():
+    class TestCamera(zoloto.cameras.marker.MarkerCamera):
+        pass
+
+    with pytest.raises(TypeError) as e:
+        TestCamera(0, marker_size=200)
+    exception_message = e.value.args[0]
+    assert (
+        "Can't instantiate abstract class TestCamera with abstract methods"
+        in exception_message
+    )
+    assert "marker_dict" in exception_message
+    assert "get_marker_size" not in exception_message
