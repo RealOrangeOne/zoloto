e6570da9c7a3f4af58f5a2e4b1836069375d0ab2
Jake Howard <RealOrangeOne@users.noreply.github.com>
2020-01-16 08:23:01 +0000

    Revert "GitHub actions"

--- tests/test_camera/test_marker_camera.py
+++ tests/test_camera/test_marker_camera.py
@@ -1,6 +1,6 @@
 import numpy
 import pytest
-from hypothesis import given, strategies
+from hypothesis import given, settings, strategies
 
 from tests.strategies import reasonable_image_size
 from zoloto.cameras.file import ImageFileCamera
@@ -19,6 +19,7 @@ def test_captures_frame_at_correct_resolution(resolution):
 
 
 @given(strategies.integers(0, 49))
+@settings(deadline=None)
 def test_detects_markers(marker_id):
     markers = list(
         MarkerCamera(
@@ -30,6 +31,7 @@ def test_detects_markers(marker_id):
 
 
 @given(strategies.integers(0, 49))
+@settings(deadline=None)
 def test_detects_marker_ids(marker_id):
     markers = MarkerCamera(
         marker_id, marker_dict=MarkerDict.DICT_6X6_50, marker_size=200
@@ -47,6 +49,7 @@ def test_sees_nothing_in_blank_image():
 
 
 @given(strategies.integers(0, 49))
+@settings(deadline=None)
 def test_eager_capture(marker_id):
     markers = list(
         MarkerCamera(
