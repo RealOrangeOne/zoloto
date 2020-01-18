476e8ca4f40b68d0e801b641ac7502eb919427e0
Jake Howard <git@theorangeone.net>
2020-01-18 15:43:27 +0000

    `_annotate_frame` mutates the frame, rather than returning it

--- examples/visual-demo.py
+++ examples/visual-demo.py
@@ -15,7 +15,7 @@ class TestCamera(Camera):
 class Viewer(CameraViewer):
     def on_frame(self, frame):
         with Timer() as annotate_timer:
-            frame = camera._annotate_frame(frame)
+            camera._annotate_frame(frame)
         print(round(annotate_timer.elapsed * 1000), end="\r")  # noqa: T001
         return frame
 
