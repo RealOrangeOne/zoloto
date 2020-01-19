8a72697f0c5145978c1728c54d85c719f6b490bb
Dan Trickey <dan@trickey.io>
2020-01-19 11:52:57 +0000

    Add stubs to marker.

--- stubs/cv2/aruco.py
+++ stubs/cv2/aruco.py
@@ -2,8 +2,13 @@
 Type stubs for cv2.aruco.
 Note that stubs are only written for the parts that we use.
 """
+from typing import List, Optional, Tuple
+
 from cv2 import aruco_DetectorParameters
+from numpy import array, ndarray
 
 
 def DetectorParameters_create() -> aruco_DetectorParameters:
     ...
+
+def estimatePoseSingleMarkers(corners: List[ndarray], markerLength: int, cameraMatrix: Optional[array], distCoeffs: Optional[array]) -> Tuple[ndarray, ndarray, ndarray]: ...
