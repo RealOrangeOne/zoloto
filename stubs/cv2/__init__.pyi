dc3964b0f93f9fe4218b0424597e29325c12517d
Dan Trickey <dan@trickey.io>
2020-01-19 00:41:02 +0000

    Add type annotations for BaseCamera
    Also some stubs for cv2

---
+++ stubs/cv2/__init__.pyi
@@ -0,0 +1,7 @@
+"""
+Type stubs for cv2.
+Note that stubs are only written for the parts that we use.
+"""
+
+class aruco_DetectorParameters:
+    pass
---
+++ stubs/cv2/aruco.py
@@ -0,0 +1,7 @@
+"""
+Type stubs for cv2.aruco.
+Note that stubs are only written for the parts that we use.
+"""
+from cv2 import aruco_DetectorParameters
+
+def DetectorParameters_create() -> aruco_DetectorParameters: ...
