05186066a2298961bb77e9e06f459ddb52961b15
Dan Trickey <dan@trickey.io>
2020-01-19 00:55:13 +0000

    Add type annotations for Camera
    Also stubs for VideoCapture
    
    The discover class methods cannot be any more strictly typed than `Any`
    as we do not know what `kwargs` any inheriting classes may be using.

--- stubs/cv2/__init__.pyi
+++ stubs/cv2/__init__.pyi
@@ -2,6 +2,18 @@
 Type stubs for cv2.
 Note that stubs are only written for the parts that we use.
 """
+from typing import Tuple, Union
+
+from numpy import ndarray
+
+CAP_PROP_BUFFERSIZE: int
 
 class aruco_DetectorParameters:
     pass
+
+class VideoCapture:
+    def __init__(self, camera_id: Union[int, str]) -> None: ...
+    def isOpened(self) -> bool: ...
+    def read(self) -> Tuple[bool, ndarray]: ...
+    def release(self) -> None: ...
+    def set(self, property: int, value: int) -> None: ...
