4baad8fe85d986c9284d77fe947235d896dd3e59
Dan Trickey <dan@trickey.io>
2020-01-19 01:05:57 +0000

    Add stubs to linting

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,14 +10,14 @@ pytest --verbose --cov zoloto/ --cov-report term --cov-report html tests/
 if hash black 2>/dev/null;
 then
     echo "> Running formatter..."
-    black zoloto tests benchmarks examples setup.py --check
+    black zoloto tests benchmarks examples setup.py stubs --check
 fi
 
 echo "> Running linter..."
-flake8 zoloto tests benchmarks examples setup.py
+flake8 zoloto tests benchmarks examples setup.py stubs
 
 echo "> Running isort..."
-isort -rc -c zoloto tests benchmarks examples setup.py
+isort -rc -c zoloto tests benchmarks examples setup.py stubs
 
 echo "> Running type checker..."
 mypy zoloto tests examples
