ac0de3bf8305b6b5efd70655e51b43ccc483c28b
Jake Howard <git@theorangeone.net>
2020-07-15 22:18:14 +0100

    Prevent passing arguments to cached method
    The argument isn't considered as part of the cache key, which is unsafe. So just remove it.

--- zoloto/utils.py
+++ zoloto/utils.py
@@ -2,7 +2,7 @@ import json
 from functools import wraps
 from typing import Any, Callable, TypeVar
 
-T = TypeVar("T", bound=Callable)
+T = TypeVar("T", bound=Callable[[Any], Any])
 
 
 def cached_method(f: T) -> T:
@@ -12,9 +12,9 @@ def cached_method(f: T) -> T:
     """
 
     @wraps(f)
-    def wrapper(obj: Any, *args: Any, **kwargs: Any) -> None:
-        value = f(obj, *args, **kwargs)
-        obj.__dict__[f.__name__] = lambda *args, **kwargs: value
+    def wrapper(obj: Any) -> Any:
+        value = f(obj)
+        obj.__dict__[f.__name__] = lambda: value
         return value
 
     wrapper.__wrapped__ = f  # type: ignore
