8f23a1d2ed7398a6e5554f177a38bfba0059ac98
Jake Howard <git@theorangeone.net>
2020-07-16 18:59:55 +0100

    Update benchmark for new setup

--- benchmarks/detection.py
+++ benchmarks/detection.py
@@ -4,34 +4,32 @@ from typing import Any, Callable
 import pytest
 
 from tests.conftest import IMAGE_DATA, TEST_IMAGE_DIR, get_calibration
-from zoloto.cameras.file import ImageFileCamera as BaseImageFileCamera
+from zoloto.cameras.file import ImageFileCamera
 from zoloto.marker_type import MarkerType
 
 
 @pytest.fixture(params=IMAGE_DATA.keys())
-def image_camera(request: Any) -> BaseImageFileCamera:
-    class ImageFileCamera(BaseImageFileCamera):
-        marker_type = MarkerType.DICT_APRILTAG_36H11
-
-        def get_marker_size(self, marker_id: int) -> int:
-            return 100
-
-    return ImageFileCamera(TEST_IMAGE_DIR.joinpath(request.param))
+def image_camera(request: Any) -> ImageFileCamera:
+    return ImageFileCamera(
+        TEST_IMAGE_DIR.joinpath(request.param),
+        marker_type=MarkerType.DICT_APRILTAG_36H11,
+        marker_size=100,
+    )
 
 
-def test_capture_frame(benchmark: Callable, image_camera: BaseImageFileCamera) -> None:
+def test_capture_frame(benchmark: Callable, image_camera: ImageFileCamera) -> None:
     benchmark(image_camera.capture_frame)
 
 
 def test_get_visible_markers(
-    benchmark: Callable, image_camera: BaseImageFileCamera
+    benchmark: Callable, image_camera: ImageFileCamera
 ) -> None:
     benchmark(image_camera.get_visible_markers)
 
 
 def test_save_frame(
     benchmark: Callable,
-    image_camera: BaseImageFileCamera,
+    image_camera: ImageFileCamera,
     temp_image_file: Callable[[str], Path],
 ) -> None:
     benchmark(image_camera.save_frame, temp_image_file)
@@ -39,7 +37,7 @@ def test_save_frame(
 
 def test_save_frame_with_annotation(
     benchmark: Callable,
-    image_camera: BaseImageFileCamera,
+    image_camera: ImageFileCamera,
     temp_image_file: Callable[[str], Path],
 ) -> None:
     benchmark(image_camera.save_frame, temp_image_file, annotate=True)
@@ -49,13 +47,11 @@ def test_save_frame_with_annotation(
 def test_process_frame(
     filename: str, benchmark: Callable, temp_image_file: Callable[[str], Path]
 ) -> None:
-    class TestCamera(BaseImageFileCamera):
-        marker_type = MarkerType.DICT_APRILTAG_36H11
-
-        def get_marker_size(self, marker_id: int) -> int:
-            return 100
-
-    camera = TestCamera(TEST_IMAGE_DIR.joinpath(filename))
+    camera = ImageFileCamera(
+        TEST_IMAGE_DIR.joinpath(filename),
+        marker_type=MarkerType.DICT_APRILTAG_36H11,
+        marker_size=100,
+    )
     benchmark(camera.save_frame, temp_image_file)
 
 
@@ -66,14 +62,10 @@ def test_process_frame_eager(
     benchmark: Callable,
     temp_image_file: Callable[[str], Path],
 ) -> None:
-    class TestCamera(BaseImageFileCamera):
-        marker_type = MarkerType.DICT_APRILTAG_36H11
-
-        def get_marker_size(self, marker_id: int) -> int:
-            return 100
-
-    camera = TestCamera(
+    camera = ImageFileCamera(
         TEST_IMAGE_DIR.joinpath(filename),
         calibration_file=get_calibration(camera_name),
+        marker_type=MarkerType.DICT_APRILTAG_36H11,
+        marker_size=100,
     )
     benchmark(camera.save_frame, temp_image_file)
