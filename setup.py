8ce4b657196fb85b452afd71a0ec8d4f1ab38618
Jake Howard <git@theorangeone.net>
2020-09-26 22:58:04 +0100

    Use regular less-than syntax

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(include="zoloto*"),
     package_data={"zoloto": ["py.typed"]},
     install_requires=[
-        "opencv-contrib-python-headless>=4.0,<=4.4",
+        "opencv-contrib-python-headless>=4.0,<4.5",
         "cached-property>=1.5",
         "pyquaternion>=0.9.2",
     ],
