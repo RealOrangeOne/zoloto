9058a6dc12ec8a5c76c96f4bc1436d46145e4252
Jake Howard <git@theorangeone.net>
2021-09-26 12:08:40 +0100

    Allow specifying marker type for preview command

--- zoloto/cli/preview.py
+++ zoloto/cli/preview.py
@@ -1,19 +1,22 @@
 import argparse
 
 from zoloto.cameras.camera import Camera
-from zoloto.marker_type import MarkerType
+from zoloto.marker_type import ALL_MARKER_TYPES, MarkerType
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--id", type=int, default=0)
+    parser.add_argument(
+        "--type",
+        type=str,
+        default=MarkerType.ARUCO_6X6_250.name,
+        choices=ALL_MARKER_TYPES,
+    )
     args = parser.parse_args()
-    with Camera(
-        args.id, marker_type=MarkerType.ARUCO_6X6_250, marker_size=100
-    ) as camera:
+    with Camera(args.id, marker_type=MarkerType[args.type], marker_size=100) as camera:
         camera.show(annotate=True)
 
 
 if __name__ == "__main__":
     main()
-2
