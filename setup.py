994d01aaaccaf5bae27dea4b0af8c59c2220c6f5
Jake Howard <git@theorangeone.net>
2021-10-01 08:23:43 +0100

    Remove runtime dependency on setuptools
    Moves some of the metadata into `setup.cfg`, so the `attr:` keyword can be used to pull the version from the live module.

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 from setuptools import find_packages, setup
 
-with open("README.md") as f:
-    readme = f.read()
-
 setup(
-    name="zoloto",
-    version="0.6.1",
-    url="https://github.com/RealOrangeOne/zoloto",
-    author="Jake Howard",
-    description="A fiducial marker system powered by OpenCV - Supports ArUco and April",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    license="BSD",
     packages=find_packages(include="zoloto*"),
     package_data={"zoloto": ["py.typed"]},
     install_requires=[
@@ -30,22 +19,4 @@ setup(
         "rpi": ["picamera[array]>=1.13"],
         "opencv": ["opencv-contrib-python>=4.0,<4.6"],
     },
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: Science/Research",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Image Recognition",
-        "Topic :: Software Development",
-        "Typing :: Typed",
-    ],
 )
