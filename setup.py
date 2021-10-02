90ee4a33a766a9d0ca73a0f6441b13ba3d6ed403
Jake Howard <git@theorangeone.net>
2021-10-02 11:13:39 +0100

    Specify all setuptools options in setup.cfg
    Turns out you can't really mix them

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    packages=find_packages(include="zoloto*"),
-    package_data={"zoloto": ["py.typed"]},
-    install_requires=[
-        "cached-property>=1.5",
-        "pyquaternion>=0.9.2",
-        "numpy<1.21",  # Requires py3.8 for correct typing https://numpy.org/devdocs/reference/typing.html
-    ],
-    project_urls={
-        "Changelog": "https://github.com/RealOrangeOne/zoloto/releases",
-        "Documentation": "https://zoloto.readthedocs.io/en/stable/",
-        "Issues": "https://github.com/RealOrangeOne/zoloto/issues",
-    },
-    entry_points={"console_scripts": ["zoloto=zoloto.cli:main"]},
-    python_requires=">=3.6",
-    extras_require={
-        "rpi": ["picamera[array]>=1.13"],
-        "opencv": ["opencv-contrib-python>=4.0,<4.6"],
-    },
-)
+setup()
