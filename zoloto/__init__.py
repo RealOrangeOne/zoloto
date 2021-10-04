3c6d710420d4ab19ec76a58fada8ac74dfd4e4cc
Jake Howard <git@theorangeone.net>
2021-10-04 08:35:14 +0100

    0.7.0

--- zoloto/__init__.py
+++ zoloto/__init__.py
@@ -2,7 +2,7 @@ from zoloto.coords import Coordinates, Orientation, Spherical, ThreeDCoordinates
 from zoloto.marker import Marker
 from zoloto.marker_type import MarkerType
 
-__version__ = "0.6.1"
+__version__ = "0.7.0"
 
 
 __all__ = [
