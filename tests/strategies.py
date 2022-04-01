b212fe756253d28d210817f2933d0a182547c18b
Jake Howard <git@theorangeone.net>
2022-04-01 08:51:09 +0100

    Update black to fix dependency issue with `click`

--- tests/strategies.py
+++ tests/strategies.py
@@ -4,6 +4,6 @@ from hypothesis import strategies
 
 from zoloto.marker_type import MAX_ALL_ALLOWED_ID, MarkerType
 
-reasonable_image_size = partial(strategies.integers, 100, 2 ** 12)
+reasonable_image_size = partial(strategies.integers, 100, 2**12)
 marker_types = partial(strategies.sampled_from, MarkerType)
 marker_ids = partial(strategies.integers, 0, MAX_ALL_ALLOWED_ID)
