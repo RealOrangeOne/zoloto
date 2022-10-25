c80921070030d254b3e1196860eb06389af85ed9
Jake Howard <git@theorangeone.net>
2022-10-25 19:05:13 +0100

    Add a test case showing issue

--- tests/test_cli/test_marker_pdfs.py
+++ tests/test_cli/test_marker_pdfs.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 
+import pytest
+from pdf2image import convert_from_path
+
+from zoloto.cameras.file import ImageFileCamera
 from zoloto.marker_type import MarkerType
 
 from . import call_cli
@@ -10,3 +14,14 @@ def test_creates_correct_number_of_pdfs(tmp_path: Path) -> None:
     rtn.check_returncode()
     pdfs = list(tmp_path.glob("*.pdf"))
     assert len(pdfs) == MarkerType.APRILTAG_16H5.marker_count
+
+
+@pytest.mark.parametrize("marker_type", MarkerType)
+def test_detectable(marker_type: MarkerType, tmp_path: Path) -> None:
+    rtn = call_cli(
+        ["marker-pdfs", marker_type.name, str(tmp_path), "100", "--range", "0"]
+    )
+    rtn.check_returncode()
+    convert_from_path(tmp_path / "0.pdf")[0].save(tmp_path / "0.png")
+    image_file_camera = ImageFileCamera(tmp_path / "0.png", marker_type=marker_type)
+    assert image_file_camera.get_visible_markers() == [0]
