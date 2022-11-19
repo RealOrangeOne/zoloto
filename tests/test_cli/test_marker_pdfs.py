c86b25a941db00cb794aa58827d5840876957d6f
Jake Howard <git@theorangeone.net>
2022-11-19 16:12:58 +0000

    Always override `minMarkerDistanceRate`
    This ensures the markers we generate are more easily detectable

--- tests/test_cli/test_marker_pdfs.py
+++ tests/test_cli/test_marker_pdfs.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-import cv2
 import pytest
 
 from zoloto.cameras.file import ImageFileCamera
@@ -25,20 +24,7 @@ def test_detectable(marker_type: MarkerType, tmp_path: Path) -> None:
     )
     rtn.check_returncode()
 
-    class CustomImageCamera(ImageFileCamera):
-        def get_detector_params(self) -> cv2.aruco_DetectorParameters:
-            """
-            Slightly tweak `minMarkerDistanceRate` to prevent the border superseding
-            the marker edge during contour refinement.
-
-            In reality, the border is removed during adaptive thresholding,
-            however this isn't possible given just the PDF page.
-            """
-            detector_params = super().get_detector_params()
-            detector_params.minMarkerDistanceRate = 0.035
-            return detector_params
-
-    image_file_camera = CustomImageCamera(tmp_path / "0.png", marker_type=marker_type)
+    image_file_camera = ImageFileCamera(tmp_path / "0.png", marker_type=marker_type)
     assert image_file_camera.get_visible_markers() == [0]
 
 
